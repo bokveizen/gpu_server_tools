efc3456801763e61283008153b626dc70fbbd089
bokveizen <boqvezen97@kaist.ac.kr>
2025-02-03 16:27:32 +0900

    colored version

--- concise_GPU_usage_monitoring/concise_smi.sh
+++ concise_GPU_usage_monitoring/concise_smi.sh
@@ -1,1 +1,5 @@
-watch -n 1 'nvidia-smi --query-gpu=index,memory.used,memory.total,utilization.gpu --format=csv,noheader,nounits | awk -F, "{print \"GPU \" \$1 \": \" \$2 \" MiB / \" \$3 \" MiB, Utilization: \" \$4 \"%\"}"'
+# watch -n 1 'nvidia-smi --query-gpu=index,memory.used,memory.total,utilization.gpu --format=csv,noheader,nounits | awk -F, "{print \"GPU \" \$1 \": \" \$2 \" MiB / \" \$3 \" MiB, Utilization: \" \$4 \"%\"}"'
+watch -c -n 1 'nvidia-smi --query-gpu=index,memory.used,memory.total,utilization.gpu --format=csv,noheader,nounits | awk -F", " '\''{
+    if (($2+0) <= 1024 && ($4+0) <= 1) { color="\033[1;32m" } else { color="\033[1;31m" }
+    printf("%sGPU %s: %s MiB / %s MiB, Utilization: %s%%\033[0m\n", color, $1, $2, $3, $4)
+}'\'''
